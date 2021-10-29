14f091fbd1e1178c3e9c277b91c7d2d3e7992e63
james <james@autotelic.io>
2021-10-29 13:19:46 +0100

    [GAME-1] randomly spawn an ingredient in a grid cell

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -52,7 +52,7 @@ public class Game : MonoBehaviour
 		{
 			var chosenRecipe = ActiveRecipes[0];
 			var ingredients = ItemUtils.RecipeMap[chosenRecipe].ToArray();
-			var ingredient = ingredients[0];
+			var ingredient = ingredients[Random.Range(0, ingredients.Length)];
 			var item = ItemUtils.ItemsMap[ingredient.NodeGUID];
 			cell.SpawnItem(item);
 		}

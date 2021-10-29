deaf58a84e29eadcdcb1066c6bc80866153ebea4
james <james@autotelic.io>
2021-10-29 15:56:05 +0100

    [GAME-1] add volume slider and save value on change

--- Assets/Scripts/Settings/SettingsMenu.cs
+++ Assets/Scripts/Settings/SettingsMenu.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+public static class Options
+{
+  public static string volume = "volume";
+}
+
 public class SettingsMenu : MonoBehaviour
 {
   [SerializeField] private RectTransform _rectTransform;
+  [SerializeField] private Slider _volumeSlider;
   
   private void Start()
   {
@@ -12,10 +18,22 @@ public class SettingsMenu : MonoBehaviour
     _rectTransform.offsetMax = new Vector2(0, 0);
     
     Toggle(false);
+
+    SetInitialOptionValues();
   }
 
   public void Toggle(bool open)
   {
     gameObject.SetActive(open);
+  }
+
+  private void SetInitialOptionValues()
+  {
+    _volumeSlider.value = PlayerPrefs.HasKey(Options.volume) ? PlayerPrefs.GetFloat(Options.volume) : 0.5f;
+  }
+
+  public void SaveVolumeSliderValue()
+  {
+    PlayerPrefs.SetFloat(Options.volume, _volumeSlider.value);
   }
 }

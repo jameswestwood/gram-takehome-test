47cc1b5c5ae2d236e251bf8278c8bb5356633742
james <james@autotelic.io>
2021-10-29 15:20:53 +0100

    [GAME-1] add settings popover

---
+++ Assets/Scripts/Settings/SettingsMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+  [SerializeField] private RectTransform _rectTransform;
+  
+  private void Start()
+  {
+    // set to default position
+    _rectTransform.offsetMin = new Vector2(0, 0);
+    _rectTransform.offsetMax = new Vector2(0, 0);
+    
+    Toggle(false);
+  }
+
+  public void Toggle(bool open)
+  {
+    gameObject.SetActive(open);
+  }
+}
